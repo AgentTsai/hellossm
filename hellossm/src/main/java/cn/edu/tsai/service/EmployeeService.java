48fa30c68546474c4286b0d1e82bc6beb16de010
AgentTsai <dhucaizhen@sina.com>
2018-01-10 21:45:01 +0800

    初始化

---
+++ hellossm/src/main/java/cn/edu/tsai/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package cn.edu.tsai.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import cn.edu.tsai.bean.Employee;
+import cn.edu.tsai.dao.EmployeeMapper;
+
+@Service
+public class EmployeeService {
+	
+	@Autowired
+	EmployeeMapper employeeMapper;
+
+	public List<Employee> getAll() {
+		// TODO Auto-generated method stub
+		return employeeMapper.selectByExampleWithDept(null);
+	}
+
+}
